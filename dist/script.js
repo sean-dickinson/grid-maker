6446f93a8a3d68bcdc7dda59b79b82a3f63b959c
Sean Dickinson <sedickinson9123@gmail.com>
2020-01-15 22:43:59 -0500

    (work) added shading features

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,15 @@
+let grid = [];
+let selectedShade = '';
+let offset;
+let size;
+let numLines;
+const canvas = document.getElementById("canvas");
+canvas.addEventListener("click", shade);
+
+
 function drawLines() {
-    const canvas = document.getElementById("canvas");
-    const numLines = parseInt(document.getElementById("numCells").value);
+    showShading();
+    numLines = parseInt(document.getElementById("numCells").value);
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
     if (canvas.width !== width || canvas.height !== height) {
@@ -9,9 +18,34 @@ function drawLines() {
     }
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const size = Math.floor(width / numLines);
-    const offset = (width % numLines) / 2;
+    size = Math.floor(width / numLines);
+    offset = (width % numLines) / 2;
     ctx.lineWidth = 1;
+    const shouldCreateGrid = grid.length !== numLines;
+    if (shouldCreateGrid) {
+        grid = [];
+    }
+    for (let i = 0; i < numLines; i++) {
+        if (shouldCreateGrid) {
+            grid.push([]);
+        }
+        for (let j = 0; j < numLines; j++) {
+            if (shouldCreateGrid) {
+                grid[i].push({
+                    x: j * size,
+                    y: size * i
+                });
+            } else {
+                grid[i][j].x = j * size;
+                grid[i][j].y = size * i;
+
+                if (grid[i][j].hasOwnProperty('fillStyle')) {
+                    shadeCell(grid[i][j]);
+                }
+            }
+        }
+    }
+
 
     for (let x = offset; x <= width - offset; x += size) {
         ctx.beginPath();
@@ -19,7 +53,6 @@ function drawLines() {
         ctx.lineTo(x, width - offset);
         ctx.stroke();
     }
-
     for (let y = offset; y <= width - offset; y += size) {
         ctx.beginPath();
         ctx.moveTo(offset, y);
@@ -33,7 +66,6 @@ function drawLines() {
 }
 
 function savepdf() {
-    const canvas = document.getElementById("canvas");
     canvas.style.visibility = "hidden";
     canvas.style.width = "8.5in";
     canvas.style.height = "11in";
@@ -50,4 +82,59 @@ function savepdf() {
     canvas.style.height = "800px";
     drawLines();
     canvas.style.visibility = "visible";
+}
+
+function showShading() {
+    document.getElementById('shading').classList.remove('hidden');
+}
+
+function selectShade(el) {
+    addSelected(el);
+    selectedShade = window.getComputedStyle(el).getPropertyValue('background-color');
+}
+
+function addSelected(el) {
+    const element = document.querySelector('.is-focused');
+    if (element) {
+        element.classList.remove('is-focused');
+    }
+    el.classList.add('is-focused');
+}
+
+function getPosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left - offset,
+        y: event.clientY - rect.top - offset
+    };
+}
+
+function shadeCell(cell) {
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = cell.fillStyle;
+    ctx.beginPath();
+    ctx.rect(cell.x + offset, cell.y + offset, size, size);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function getCell(pos) {
+    const x = Math.floor(pos.x / size);
+    const y = Math.floor(pos.y / size);
+    const cell = grid[y][x];
+    return cell;
+
+
+}
+
+
+function shade(event) {
+
+    const pos = getPosition(event);
+    // console.log(pos);
+    const cell = getCell(pos);
+    cell.fillStyle = selectedShade;
+    shadeCell(cell);
+
+
 }
